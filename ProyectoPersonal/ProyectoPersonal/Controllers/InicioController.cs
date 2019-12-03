f839559049037255b703f09255a72823c104203a
AlumnoMCSD <AlumnoMCSD@T06W03>
2019-12-03 14:26:43 +0100

    Boostrap nuevo Proyecto

---
+++ ProyectoPersonal/ProyectoPersonal/Controllers/InicioController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoPersonal.Controllers
+{
+    public class InicioController : Controller
+    {
+        // GET: Inicio
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Pagina()
+        {
+            return View();
+        }
+    }
+}
